b7f6c97413cdcb518a8a91e81c49f24d0e3d24ac
michael <952419245@qq.com>
2018-10-27 15:34:38 +0800

    Michael

--- app/src/main/java/com/miracle/sport/SportMainActivity.java
+++ app/src/main/java/com/miracle/sport/SportMainActivity.java
@@ -4,11 +4,12 @@ import android.view.View;
 
 import com.miracle.R;
 import com.miracle.base.BaseActivity;
+import com.miracle.databinding.ActivitySportMainBinding;
 
 /**
  * Created by Michael on 2018/10/27 13:32 (星期六)
  */
-public class SportMainActivity extends BaseActivity {
+public class SportMainActivity extends BaseActivity<ActivitySportMainBinding> {
     @Override
     public int getLayout() {
         return R.layout.activity_sport_main;
